c468104b8b9c66b1d5085ef314ddfc8c4dc2359e
weiyaoke <weiyaoke08@gmail.com>
2025-07-20 21:57:38 +0800

    ai

--- ai-media/src/main/java/com/tianji/media/constants/FileConstants.java
+++
@@ -1,6 +0,0 @@
-package com.tianji.media.constants;
-
-public interface FileConstants {
-
-    long MEDIA_FREE_SECONDS = 300;
-}
--- ai-media/src/main/java/com/tianji/media/storage/tencent/UserAccessInfo.java
+++
@@ -1,9 +0,0 @@
-package com.tianji.media.storage.tencent;
-
-import lombok.Data;
-
-@Data
-public class UserAccessInfo {
-    private Long exper;
-    private String uid;
-}
